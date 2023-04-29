8442a43ec5fcaf0a2d21da51c2dffb940940733d
고운 <kgukgu33@gmail.com>
2023-04-29 12:21:34 +0900

    [unrated] Title: 0 떼기, Time: 0.02 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/unrated/181847. 0 떼기/0 떼기.java
@@ -1,10 +1,8 @@
 class Solution {
     public String solution(String n_str) {
         StringBuilder sb=new StringBuilder(n_str);
-        int firstNum=sb.charAt(0)-'0';
-        while(firstNum==0){
+        while(sb.charAt(0)=='0'){
             sb.deleteCharAt(0);
-            firstNum=sb.charAt(0)-'0';
         }
         return sb.toString();
     }
