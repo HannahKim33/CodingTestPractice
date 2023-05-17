d4d1601fea244b2e674bf1abb534e088d17ccea5
고운 <kgukgu33@gmail.com>
2023-05-17 23:14:39 +0900

    [level 0] Title: 숫자 찾기, Time: 6.23 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,18 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
-        boolean found=false;
-        String str_num=String.valueOf(num);
-        for(int i=0; i<str_num.length(); i++){
-            if(str_num.charAt(i)==k+'0'){
-                answer=i+1;
-                found=true;
-                break;
-            }
-        }
-        if(!found){
-            answer=-1;
-        }
-        return answer;
+        return (" "+num).indexOf(String.valueOf(k));
     }
 }
